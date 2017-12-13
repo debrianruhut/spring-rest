249945f8461c2075e9214b66c14d0f2b99a9d215
Debrian Ruhut Saragih <debrianruhut.saragih@gmail.com>
2017-12-13 11:55:19 +0700

    Commit 4

--- src/main/java/hello/GreetingController.java
+++ src/main/java/hello/GreetingController.java
@@ -14,14 +14,24 @@ import java.util.List;
 import java.util.concurrent.atomic.AtomicLong;
 import java.util.stream.Collectors;
 
+import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import hello.model.Greeting;
+import hello.model.TblGuru;
+import hello.model.TblMataPelajaran;
+import hello.model.TblSiswa;
+
 @RestController
+@RequestMapping("/great")
 public class GreetingController {
 
     private static final String template = "Hello, %s!";
@@ -32,6 +42,92 @@ public class GreetingController {
     @Autowired
     private EntityManagerFactory em;
     
+    @PostMapping("/addGuru")
+    public int addGuru() {
+    	int hasil = 0;
+		try {
+			EntityManager e = em.createEntityManager();
+			e.getTransaction().begin();
+			
+			TblGuru guru = new TblGuru();
+			guru.setNip("198906012012101002");
+			guru.setPassword("Dosen");
+			guru.setNmGuru("Suminah");
+			guru.setAlamat("Jalan Pemuda");
+			guru.setTempatLahir("Jakarta");
+			guru.setTanggalLahir("11-11-2011");
+			guru.setJenisKelamin("Perempuan");
+			guru.setFoto("test.jpg");
+			guru.setTelpon("02192382");
+			guru.setAgama("Kristen");
+			guru.setJabatan("Guru KWN");
+			guru.setGol("IV/a");
+			guru.setTamatan("SI/AIV/PMP/90");
+			guru.setLevel("dosen");
+			
+			e.persist(guru);
+			e.getTransaction().commit();
+		} catch (Exception ex) {
+			System.out.println(ex.getMessage());
+			hasil = -1;
+		}
+		return hasil;
+    }
+    
+    @GetMapping("/editGuru")
+	  public int editGuru(@RequestParam("nip") String nip) {
+	  	int hasil = 0;
+			try {
+				
+				EntityManager e = em.createEntityManager();
+				e.getTransaction().begin();
+				
+				TblGuru guru = e.find(TblGuru.class, nip);
+				guru.setNip("198906012012101002");
+				guru.setPassword("Dosen 1");
+				guru.setNmGuru("Suminah 1");
+				guru.setAlamat("Jalan Pemuda 1");
+				guru.setTempatLahir("Jakarta 1");
+				guru.setTanggalLahir("11-11-2011");
+				guru.setJenisKelamin("Perempuan 1");
+				guru.setFoto("test.jpg");
+				guru.setTelpon("02192382");
+				guru.setAgama("Kristen");
+				guru.setJabatan("Guru KWN 2");
+				guru.setGol("IV/a");
+				guru.setTamatan("SI/AIV/PMP/90");
+				guru.setLevel("dosen 1");
+
+				e.getTransaction().commit();
+			} catch (Exception ex) {
+				System.out.println(ex.getMessage());
+				hasil = -1;
+			}
+			return hasil;
+	  }
+    @PostMapping("/addMapel")
+    public int addMapel() {
+    	int hasil = 0;
+		try {
+			EntityManager e = em.createEntityManager();
+			e.getTransaction().begin();
+			
+			TblMataPelajaran mapel = new TblMataPelajaran();
+			mapel.setKdPelajaran("1217");
+			mapel.setNmMapel("Bahasa Inggris");
+			mapel.setNip("198906012012101006");
+
+
+			e.persist(mapel);
+			e.getTransaction().commit();
+		} catch (Exception ex) {
+			System.out.println(ex.getMessage());
+			hasil = -1;
+		}
+		return hasil;
+    }    
+    
+    @CrossOrigin(origins= {"*"})
     @RequestMapping("/guru")
     public List<TblGuru> allGurus(){
     	return em .createEntityManager().createQuery("from TblGuru").getResultList();
